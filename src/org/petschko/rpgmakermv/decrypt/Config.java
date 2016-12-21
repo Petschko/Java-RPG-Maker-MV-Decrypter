5f508cab72b894edb052301665e0cca119369d04
Petschko <peter-91@hotmail.de>
2016-12-21 23:54:25 +0100

    Added Config-Class

---
+++ src/org/petschko/rpgmakermv/decrypt/Config.java
@@ -0,0 +1,21 @@
+package org.petschko.rpgmakermv.decrypt;
+
+/**
+ * Author: Peter Dragicevic [peter-91@hotmail.de]
+ * Authors-Website: http://petschko.org/
+ * Date: 11.12.2016
+ * Time: 21:24
+ * Update: -
+ * Version: 0.0.1
+ *
+ * Notes: Config class
+ */
+class Config {
+	// Program Info
+	static final String creator = "Petschko";
+	static final String version = "v0.1 Alpha";
+	static final String programmName = "RPG-Maker MV Decrypter";
+
+	// System-Settings
+	static final String ds = System.getProperty("file.separator");
+}
