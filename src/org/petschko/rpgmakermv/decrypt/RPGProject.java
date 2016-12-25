902f64289a1b743c23ac80b4e83e6b77b24201b3
Petschko <peter-91@hotmail.de>
2016-12-26 00:02:52 +0100

    Added many functions to the RPGProject Class

--- src/org/petschko/rpgmakermv/decrypt/RPGProject.java
+++ src/org/petschko/rpgmakermv/decrypt/RPGProject.java
@@ -1,7 +1,11 @@
 package org.petschko.rpgmakermv.decrypt;
 
 import com.sun.istack.internal.NotNull;
+import org.json.JSONException;
+import sun.dc.path.PathException;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -18,17 +22,28 @@ public class RPGProject {
 	private String path;
 	private String outputPath = Config.defaultOutputDir;
 	private File system = null;
+	private String encryptionKeyName = "encryptionKey";
+	private boolean isEncrypted = true;
+	private ArrayList<File> files = new ArrayList<>();
 	private ArrayList<File> encryptedFiles = new ArrayList<>();
 
 	/**
 	 * RPGProject Constructor
 	 *
 	 * @param path - Path to the RPG-Maker-Project
+	 * @throws PathException - Path doesn't exists exception
 	 */
-	public RPGProject(@NotNull String path) {
+	public RPGProject(@NotNull String path) throws PathException {
+		if(Files.notExists(Paths.get(path)))
+			throw new PathException("Project-Path doesn't exists!");
+
 		this.setPath(path);
+		this.loadFiles();
 		this.findSystemFile();
-		this.findEncryptedFiles();
+		this.checkIfEncrypted();
+
+		if(this.isEncrypted())
+			this.findEncryptedFiles();
 	}
 
 	/**
@@ -45,7 +60,7 @@ public class RPGProject {
 	 *
 	 * @param path - Path of the Project
 	 */
-	public void setPath(@NotNull String path) {
+	private void setPath(@NotNull String path) {
 		this.path = path;
 	}
 
@@ -63,7 +78,7 @@ public class RPGProject {
 	 *
 	 * @param outputPath - Output (Save-Dir)-Path of the Project
 	 */
-	public void setOutputPath(String outputPath) {
+	public void setOutputPath(@NotNull String outputPath) {
 		this.outputPath = outputPath;
 	}
 
@@ -86,19 +101,139 @@ public class RPGProject {
 	}
 
 	/**
+	 * Returns the EncryptionKeyName
+	 *
+	 * @return - EncryptionKeyName
+	 */
+	public String getEncryptionKeyName() {
+		return encryptionKeyName;
+	}
+
+	/**
+	 * Sets the EncryptionKeyName
+	 *
+	 * @param encryptionKeyName - EncryptionKeyName
+	 */
+	public void setEncryptionKeyName(String encryptionKeyName) {
+		this.encryptionKeyName = encryptionKeyName;
+	}
+
+	/**
+	 * Set the EncryptionKeyName to the Default-Value
+	 */
+	public void setEncryptionKeyNameToDefault() {
+		this.setEncryptionKeyName("encryptionKey");
+	}
+
+	/**
+	 * Returns true if Project is encrypted
+	 *
+	 * @return - true if Project is encrypted else false
+	 */
+	public boolean isEncrypted() {
+		return isEncrypted;
+	}
+
+	/**
+	 * Sets to true if the Project is Encrypted
+	 *
+	 * @param encrypted - true if the Project is encrypted else false
+	 */
+	private void setEncrypted(boolean encrypted) {
+		isEncrypted = encrypted;
+	}
+
+	/**
+	 * Returns the File List of the Project
+	 *
+	 * @return - File List
+	 */
+	public ArrayList<File> getFiles() {
+		return files;
+	}
+
+	/**
 	 * Returns the Encryption-File List
 	 *
-	 * @return - Encryption-File-List
+	 * @return - Encryption-File List
 	 */
 	public ArrayList<File> getEncryptedFiles() {
 		return encryptedFiles;
 	}
 
+	/**
+	 * Load all Files of the Project into an ArrayList
+	 */
+	private void loadFiles() {
+		java.io.File projectPath = new java.io.File(this.getPath());
+		ArrayList<java.io.File> files = File.readDirFiles(projectPath);
+
+		for(java.io.File file : files) {
+			try {
+				this.getFiles().add(new File(file.getCanonicalPath()));
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Checks if the Encryption-Key is Found within the System-File
+	 */
+	private void checkIfEncrypted() {
+		Decrypter d = new Decrypter();
+
+		try {
+			d.detectEncryptionKey(this.getSystem(), this.getEncryptionKeyName()); // todo later: add more default values and test them
+		} catch(JSONException e) {
+			this.setEncrypted(false);
+		} finally {
+			this.setEncrypted(true);
+		}
+	}
+
+	/**
+	 * Find all Encrypted-Files of the Project and save them into an ArrayList
+	 */
 	private void findEncryptedFiles() {
-		// todo implement
+		if(! this.isEncrypted())
+			return;
+
+		for(File file : this.getFiles()) {
+			if(file.getExtension().equals("rpgmvp") || file.getExtension().equals("rpgmvm") || file.getExtension().equals("rpgmvo")) // todo later: add more default values and test them
+				this.getEncryptedFiles().add(file);
+		}
 	}
 
 	private void findSystemFile() {
-		// todo implement
+		// todo implement find system.json
+	}
+
+	public void decryptFiles(boolean ignoreFakeHeader) throws JSONException {
+		// todo implement decryption through encrypted files array and save them to the output location
+		Decrypter decrypter = new Decrypter();
+
+		decrypter.detectEncryptionKey(this.getSystem(), this.getEncryptionKeyName());
+		decrypter.setIgnoreFakeHeader(ignoreFakeHeader);
+
+		for(int i = 0; i < this.getEncryptedFiles().size(); i++) {
+			File currentFile = this.getEncryptedFiles().get(i);
+
+			try {
+				decrypter.decryptFile(currentFile);
+			} catch(Exception e) {
+				e.printStackTrace();
+			} finally {
+				currentFile.changePathToFile(this.getOutputPath());
+				currentFile.save();
+				currentFile.unloadContent();
+			}
+		}
+	}
+
+	public boolean restoreProjectFile() {
+		// todo implement create the project file and check if all is on the right spot and decrypted
+
+		return false;
 	}
 }
