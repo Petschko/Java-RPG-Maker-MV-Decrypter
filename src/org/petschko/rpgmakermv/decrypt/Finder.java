12cb8f6a0874584da858f06ba379e41df77f4b8c
Petschko <peter-91@hotmail.de>
2016-12-26 01:24:18 +0100

    Added Finder-Class

---
+++ src/org/petschko/rpgmakermv/decrypt/Finder.java
@@ -0,0 +1,96 @@
+package org.petschko.rpgmakermv.decrypt;
+
+import org.json.JSONException;
+
+/**
+ * Author: Peter Dragicevic [peter-91@hotmail.de]
+ * Authors-Website: http://petschko.org/
+ * Date: 26.12.2016
+ * Time: 00:02
+ * Update: -
+ * Version: 0.0.1
+ *
+ * Notes: Finder Class
+ */
+class Finder {
+	/**
+	 * Constructor
+	 */
+	private Finder() {
+		// VOID - This is a Static-Class
+	}
+
+	/**
+	 * Search all know places for the System-File
+	 *
+	 * @param projectDir - Directory of the Project
+	 * @return - System-File-Object if found else null
+	 */
+	public static File findSystemFile(String projectDir) {
+		String[] filePaths = new String[]{
+				"data" + Config.ds + "System.json",
+				"www" + Config.ds + "data" + Config.ds + "System.json"
+		};
+		File systemJsonFile = null;
+
+		for(String filePath : filePaths) {
+			java.io.File systemFile = new java.io.File(projectDir + filePath);
+
+			if(systemFile.exists()) {
+				try {
+					systemJsonFile = new File(projectDir + filePath);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+
+				return systemJsonFile;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Check if the given Extension is an Encrypted Extension
+	 *
+	 * @param extension - File-Extension to check
+	 * @return - true if the Extension is an encrypted File-extension else false
+	 */
+	public static boolean isFileEncryptedExt(String extension) {
+		switch(extension) {
+			case "rpgmvp":
+			case "rpgmvm":
+			case "rpgmvo":
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	/**
+	 * Check if Encryption-Key name is may different from given - Tests all know Encryption-Key Names
+	 *
+	 * @param systemFile - System.json File
+	 * @return - Key-Name or null if not found
+	 */
+	public static String testEncryptionKeyNames(File systemFile) {
+		String[] keyNames = new String[]{"encryptionKey"};
+		Decrypter d = new Decrypter();
+		String result = null;
+
+		for(String keyName : keyNames) {
+			try {
+				d.detectEncryptionKey(systemFile, keyName);
+
+				if(d.getDecryptCode() != null) {
+					result = keyName;
+					break;
+				}
+			} catch(JSONException e) {
+				// Void
+			}
+		}
+
+		return result;
+	}
+}
