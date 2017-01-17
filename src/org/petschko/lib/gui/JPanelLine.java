3ecc8d5c124700f0881bad04f41bff783cc68a1d
Petschko <peter-91@hotmail.de>
2017-01-17 20:11:17 +0100

    Added JPanelLine Class

---
+++ src/org/petschko/lib/gui/JPanelLine.java
@@ -0,0 +1,34 @@
+package org.petschko.lib.gui;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Component;
+
+/**
+ * Author: Peter Dragicevic [peter-91@hotmail.de]
+ * Authors-Website: http://petschko.org/
+ * Date: 17.01.2017
+ * Time: 18:52
+ * Update: -
+ * Version: 0.0.1
+ *
+ * Notes: JPanelLine Class
+ */
+public class JPanelLine extends JPanel {
+	/**
+	 * Creates a new <code>JPanel</code> with a double buffer
+	 * and a box layout.
+	 */
+	public JPanelLine() {
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.setAlignmentX(Component.LEFT_ALIGNMENT);
+	}
+
+	/**
+	 * Adds a Label with 1 empty space
+	 */
+	public void addSpaceLabel() {
+		this.add(new JLabel(" "));
+	}
+}
