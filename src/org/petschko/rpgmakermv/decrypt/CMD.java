4892174a812e5d36bc77fd9a4fa38d95cc907122
Petschko <peter@petschko.org>
2021-02-19 13:59:59 +0100

    Added help alias (help [command])

--- src/org/petschko/rpgmakermv/decrypt/CMD.java
+++ src/org/petschko/rpgmakermv/decrypt/CMD.java
@@ -118,8 +118,13 @@ class CMD {
 				// Help is handles a bit different
 				if(invalidCommand)
 					cmdHandler.run(args);
-				else
-					cmdHandler.printHelp();
+				else {
+					// Check if 2nd param is command so make this an alias too
+					if(subCommand != null) {
+						printHelpForCmd(subCommand);
+					} else
+						cmdHandler.printHelp();
+				}
 
 				exitCMD(invalidCommand ? STATUS_WARNING : STATUS_OK);
 				return;
@@ -141,6 +146,44 @@ class CMD {
 			return false;
 
 		return command.equals(CMD_HELP) || command.equals(CMD_HELP_2) || command.equals(CMD_HELP_3) || command.equals(CMD_HELP_4);
+	}
+
+	/**
+	 * Prints out the help for a specific command
+	 *
+	 * @param command - Command to print help of
+	 */
+	private void printHelpForCmd(String command) {
+		CMD_Command cmdCommand = new CMD_Help();
+
+		switch(command) {
+			case CMD_OPEN_IMG:
+				cmdCommand = new CMD_Open();
+				break;
+			case CMD_DECRYPT:
+				cmdCommand = new CMD_Decrypt();
+				break;
+			case CMD_ENCRYPT:
+				cmdCommand = new CMD_Encrypt();
+				break;
+			case CMD_RESTORE:
+				cmdCommand = new CMD_Restore();
+				break;
+			case CMD_RESTORE_PROJECT:
+				cmdCommand = new CMD_RestoreProject();
+				break;
+			case CMD_GET_KEY:
+			case CMD_GET_KEY_2:
+			case CMD_GET_KEY_3:
+			case CMD_GET_KEY_4:
+			case CMD_GET_KEY_5:
+				cmdCommand = new CMD_DetectKey();
+				break;
+			default:
+				// Void
+		}
+
+		cmdCommand.printHelp();
 	}
 
 	/**
